e350ed50074fdbb9917db541d94711a00909e868
Максим Арзамасцев <kyhnya@yandex.ru>
2021-09-30 18:33:49 +0300

    Calculator_after_second_review

--- homework.py
+++ homework.py
@@ -12,8 +12,7 @@ class Record:
         self.amount = amount
         self.comment = comment
         if date is None:
-            self.now = dt.datetime.now()
-            self.date = self.now.date()
+            self.date = dt.date.today()
         else:
             datetime = dt.datetime.strptime(date, self.DATE_FORMAT)
             self.date = datetime.date()
@@ -21,80 +20,70 @@ class Record:
 
 class Calculator:
     ''' Подсчитывает общее кол-во денег и калорий.'''
+    TIME_DELTA = dt.timedelta(days=7)
+
     def __init__(self, limit):
         self.limit = limit
         self.records = []
-        self.today = dt.date.today()
-        self.period = dt.timedelta(days=7)
 
     def add_record(self, note):
-        '''Сохраняет новую запись о
-           расходах/приёме пищи.
-        '''
         self.records.append(note)
 
     def get_today_stats(self):
-        '''Cчитает сколько денег/калорий
-           потрачено/съедено уже сегодня.
-        '''
-        day = []
-        for note in self.records:
-            if note.date == self.today:
-                day.append(note.amount)
-        return sum(day)
+        today = dt.date.today()
+        return sum(note.amount for note in self.records if note.date == today)
 
     def get_week_stats(self):
-        '''Считает сколько денег/калорий
-           потрачено/получено за 7 дней.
-        '''
         week = []
-        sennight = self.today - self.period
+        today = dt.date.today()
+        sennight = today - self.TIME_DELTA
         for note in self.records:
-            if sennight <= note.date <= self.today:
+            if sennight < note.date <= today:
                 week.append(note.amount)
         return sum(week)
 
 
 class CaloriesCalculator(Calculator):
     '''Подсчитывает кол-во калорий.'''
+    PHRASE = ('Сегодня можно съесть что-нибудь ещё, '
+              'но с общей калорийностью не более {key_to_insert} кКал')
+
     def get_calories_remained(self):
-        '''Определяет сколько ещё калорий
-           можно/нужно получить сегодня.
-        '''
         calories = self.limit - self.get_today_stats()
         if calories > 0:
-            return (f'Сегодня можно съесть что-нибудь ещё, '
-                    f'но с общей калорийностью не более {calories} кКал')
+            return self.PHRASE.format(key_to_insert=calories)
         return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
     ''' Подсчитывает кол-во денег.'''
-    USD_RATE = 72.97
-    EURO_RATE = 85.50
+    USD_RATE = 60.0
+    EURO_RATE = 70.0
+    RUB_RATE = 1.0
+    SHORT_PHRASE = 'Денег нет, держись'
+    MIDDLE_PHRASE = 'На сегодня осталось {key_first} {key_second}'
+    LONG_PHRASE = 'Денег нет, держись: твой долг - {key_first} {key_second}'
+    CURRENCIES = {'rub': ('руб', RUB_RATE),
+                  'usd': ('USD', USD_RATE),
+                  'eur': ('Euro', EURO_RATE)}
 
     def get_today_cash_remained(self, currency):
-        '''Определяет сколько ещё денег можно потратить
-           сегодня в рублях, долларах, или евро.
-        '''
-        moneys = {'rub': 'руб',
-                  'usd': 'USD',
-                  'eur': 'Euro'}
-        value = moneys[currency]
-        if currency not in moneys:
-            return 'Не указана валюта!'
+        if currency not in self.CURRENCIES:
+            raise ValueError("Несоответствующее значение")
+        currency_name, currency_rate = self.CURRENCIES[currency]
         balance = self.limit - self.get_today_stats()
-        if currency == 'rub':
-            balance = round(balance, 2)
-        elif currency == 'usd':
-            balance = round(balance / self.USD_RATE, 2)
-        elif currency == 'eur':
-            balance = round(balance / self.EURO_RATE, 2)
-        else:
-            balance = 0
+        balance = round(balance / currency_rate, 2)
         if balance == 0:
-            return 'Денег нет, держись'
+            return self.SHORT_PHRASE
         if balance > 0:
-            return (f'На сегодня осталось {balance} {value}')
+            return self.MIDDLE_PHRASE.format(key_first=balance,
+                                             key_second=currency_name)
         balance = abs(balance)
-        return (f'Денег нет, держись: твой долг - {balance} {value}')
+        return self.LONG_PHRASE.format(key_first=balance,
+                                       key_second=currency_name)
+
+
+cash = CashCalculator(1000)
+cash.add_record(Record(amount=145, comment='кофе'))
+cash.add_record(Record(amount=300, comment='Серёге за обед'))
+print(cash.get_today_cash_remained('rub'))
